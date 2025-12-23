24b943fcc170e1139e835b9c63ecfa888220d21b
lone <lonechan314@qq.com>
2025-12-23 20:00:33 +0800

    feat: admin service auth middleware & config
    Signed-off-by: lone <lonechan314@qq.com>

--- cascade-oj/app/services/admin/internal/server/http.go
+++ cascade-oj/app/services/admin/internal/server/http.go
@@ -4,6 +4,7 @@ import (
 	pb "cascade-oj/api/cascade/admin/v1"
 	"cascade-oj/app/services/admin/internal/conf"
 	"cascade-oj/app/services/admin/internal/service"
+	"cascade-oj/pkg/middleware/auth"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
@@ -15,6 +16,7 @@ func NewHTTPServer(c *conf.Server, admin *service.AdminService, logger log.Logge
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
+			auth.Auth(c.JwtSecret, auth.RoleAdmin, nil),
 		),
 	}
 	if c.Http.Network != "" {
