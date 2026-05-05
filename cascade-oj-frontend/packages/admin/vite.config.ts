d8e5db547aced7bfb861253daf0a11200474fa42
lone <lonechan314@qq.com>
2026-05-05 20:36:57 +0800

    amend: discard changes
    Signed-off-by: lone <lonechan314@qq.com>

--- cascade-oj-frontend/packages/admin/vite.config.ts
+++ cascade-oj-frontend/packages/admin/vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
             '/api': {
                 target: 'http://localhost:10086',
                 changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
+                // rewrite: (path) => path.replace(/^\/api/, '')
             }
         }
     }
